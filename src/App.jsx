d69316f759ea31906a2318f382a77ce45eca78bc
se7enwin <se7enwin@hotmail.com>
2025-05-10 19:20:56 +0200

    Se genera Nav, Perfil y agregra BrowserRouter en index

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Nav from './Component/Nav/Nav'
+import Perfil from './Component/Perfil/Perfil'
+import { Route, Routes, useLocation, useNavegation } from 'react-router-dom';
 export default function App() {
 
     return (
 
-        <h3>Componente Función desde Jsx - Local </h3>
+        <div>
+            <h3>Componente Función desde Jsx - Local </h3>
+            <hr />
+            <Nav />
+            <Routes>
+                <Route path='/perfil' element={<Perfil />} />
+
+
+            </Routes>
+        </div>
+
+
     )
 }
---
+++ src/Component/Nav/Nav.jsx
@@ -0,0 +1,8 @@
+
+export default function Nav() {
+
+    return (
+
+        <h3> Barra de Navegación </h3>
+    )
+}
